fd7ef40053ea2ef724da45a45e4e1e130311eb79
Matysz11 <mateusz.but123@gmail.com>
2025-11-18 16:41:52 +0100

    jaki gang wgl, spawner działa jako tako do roboty potem jak mapa będzie czy coś

--- giereczka/main.gd
+++ giereczka/main.gd
@@ -3,7 +3,19 @@ extends Node3D
 
 @onready var target = $player
 
+var EnemyScene = preload("res://enemy1.tscn")
+
+func spawn_enemy(position: Vector3):
+	var enemy = EnemyScene.instantiate()
+	enemy.global_transform.origin = position
+	add_child(enemy)
+
+func _ready():
+	spawn_enemy(Vector3(-10, 0, -10))
+	spawn_enemy(Vector3(-10, 0, 0))
+	spawn_enemy(Vector3(-10, 0, 10))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	get_tree().call_group("enemy", "target_position", target.global_transform.origin)
