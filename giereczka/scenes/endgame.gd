67292c66ced8d1b1289e1f97b2eb1c9d89cbcadc
Janek <jan.szeszo@infotech.edu.pl>
2026-05-25 21:43:08 +0200

    DOdano WIn/Lose screen

--- giereczka/scenes/endgame.gd
+++ giereczka/scenes/endgame.gd
@@ -1,24 +1,28 @@
 extends Control
 
 var do_win = null
-
+var played_sound = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	do_win = Global.do_win
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	# Make sure the mouse is visible so the player can click the buttons
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if do_win:
+	
+	if Global.do_win:
 		$win.visible = true
+		$lose.visible = false
+		$WinSound.play()
 	else:
+		$win.visible = false
 		$lose.visible = true
-
+		$LoseSound.play() 
 
 func _on_new_game_pressed() -> void:
 	Global.load_game = false
 	get_tree().change_scene_to_file("res://main.tscn")
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
