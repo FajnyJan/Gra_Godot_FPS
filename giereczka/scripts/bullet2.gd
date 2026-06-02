057930a1bba866028b426462b8ba7694864cf9c6
Matysz11 <mateusz.but123@gmail.com>
2026-06-02 13:30:13 +0200

    bomby niewidzialnie wybuchają

--- giereczka/scripts/bullet2.gd
+++ giereczka/scripts/bullet2.gd
@@ -5,9 +5,11 @@ var direction = Vector3.ZERO
 var damage = 3
 var lifetime = 10.0
 
+@onready var explosion_area = $Area3D
+
 func _ready():
 	await get_tree().create_timer(lifetime).timeout
-	queue_free()
+	explode()
 
 func _physics_process(delta):
 	if direction != Vector3.ZERO:
@@ -15,8 +17,14 @@ func _physics_process(delta):
 		rotate_y(deg_to_rad(180))
 	var collision = move_and_collide(direction * speed * delta)
 	if collision:
-		var body = collision.get_collider()
+		explode()
+
+func explode():
+	var bodies = explosion_area.get_overlapping_bodies()
+
+	for body in bodies:
 		if body.is_in_group("enemy"):
 			if body.has_method("apply_damage"):
 				body.apply_damage(damage)
-			queue_free()
+
+	queue_free()
